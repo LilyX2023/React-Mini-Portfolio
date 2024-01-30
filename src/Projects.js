3c8057c9603617d751bd12dc4d54867ee36c8c10
RachelY <yang.yijing@outlook.com>
2024-01-30 01:20:45 -0500

    Flexcard for project added to the page

--- src/Projects.js
+++ src/Projects.js
@@ -2,11 +2,30 @@ const Projects = (props) => {
 
     //The Components Returned JSX
     return <div className="Projects">
-        <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h2>
-        <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h2>
-        <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h2>
-        <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h2>
-        <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h2>
+    <div class='card1'>
+        <div class = 'project-sidebar'>
+            <img src='https://thumbs.dreamstime.com/b/museum-logo-trendy-minimal-linear-style-vector-icon-bank-building-columns-simple-emblem-university-isolated-177740902.jpg' alt=""/>
+
+            <a href="https://github.com/LilyX2023/sealproject1"><i class="fa-brands fa-github"></i></a>
+        </div>
+        <div class = 'project-main'>
+        <h2 class="project-name">ArtWalk</h2>
+        <p class="project-description"> An app can retrieve the information of the art galleries that are in the Harvard Art Museums building on each floor. </p> 
+        </div>
+
+    </div>
+    <div class='card2'>
+        <div class = 'project-sidebar'>
+            <img src='https://static.vecteezy.com/system/resources/previews/014/662/247/non_2x/job-search-line-icon-vector.jpg' alt=""/>
+
+            <a href="https://github.com/LilyX2023/project2"><i class="fa-brands fa-github"></i></a>
+        </div>
+        <div class = 'project-main'>
+        <h2 class="project-name">CareerCanvas</h2>
+        <p class="project-description"> An app to help users to document their job searching process. Users will be able to create, update, and delete the positions they applied for. </p> 
+        </div>
+
+    </div>
     </div>
 }
 
